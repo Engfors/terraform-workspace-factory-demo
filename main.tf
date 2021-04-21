979418353e15d45030ec01c4c09e628a9c0914ff
Emil Engfors <engfors.emil@gmail.com>
2021-04-21 12:25:52 +0200

    demo cleanup

--- main.tf
+++ main.tf
@@ -1,42 +1,42 @@
-module "demo_workspaces" {
-  for_each = var.demo_workspaces
-  source   = "app.terraform.io/engfors/workspace/terraform"
-  version  = "1.1.0"
+# module "demo_workspaces" {
+#   for_each = var.demo_workspaces
+#   source   = "app.terraform.io/engfors/workspace/terraform"
+#   version  = "1.1.0"
 
-  vcs = true
+#   vcs = true
 
-  workspace = [
-    {
-      name                  = each.key
-      organization          = var.organization
-      auto_apply            = false
-      file_triggers_enabled = true
-      operations            = true
-      ssh_key_id            = ""
-      trigger_prefixes      = each.value.vcs_path
-      working_directory     = each.value.vcs_path
-      tf_version            = var.tf_version
-      queue_all_runs        = false
-      identifier            = each.value.vcs_repo
-      branch                = ""
-      ingress_submodules    = false
-      oauth_token_id        = var.github_oauth_token_id
-    }
-  ]
+#   workspace = [
+#     {
+#       name                  = each.key
+#       organization          = var.organization
+#       auto_apply            = false
+#       file_triggers_enabled = true
+#       operations            = true
+#       ssh_key_id            = ""
+#       trigger_prefixes      = each.value.vcs_path
+#       working_directory     = each.value.vcs_path
+#       tf_version            = var.tf_version
+#       queue_all_runs        = false
+#       identifier            = each.value.vcs_repo
+#       branch                = ""
+#       ingress_submodules    = false
+#       oauth_token_id        = var.github_oauth_token_id
+#     }
+#   ]
 
-  workspace_variables = [
-    {
-      key       = "google_project"
-      value     = each.value.google_project
-      category  = "terraform"
-      hcl       = "false"
-      sensitive = "false"
-    }
-  ]
+#   workspace_variables = [
+#     {
+#       key       = "google_project"
+#       value     = each.value.google_project
+#       category  = "terraform"
+#       hcl       = "false"
+#       sensitive = "false"
+#     }
+#   ]
 
-  notification_configuration = [
-    {
-      notification = false
-    }
-  ]
-}
+#   notification_configuration = [
+#     {
+#       notification = false
+#     }
+#   ]
+# }
